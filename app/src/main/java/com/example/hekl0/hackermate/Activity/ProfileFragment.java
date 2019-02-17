669bf78c61e4cbce6211151ad3ae63db29b1cd10
Ash Nguyen <atn4403@rit.edu>
2019-02-16 21:43:38 -0500

    create grid view, TODO: implement grid view

--- app/src/main/java/com/example/hekl0/hackermate/Activity/ProfileFragment.java
+++ app/src/main/java/com/example/hekl0/hackermate/Activity/ProfileFragment.java
@@ -6,8 +6,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.hekl0.hackermate.R;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import jp.wasabeef.picasso.transformations.CropCircleTransformation;
+import jp.wasabeef.picasso.transformations.CropSquareTransformation;
+import jp.wasabeef.picasso.transformations.CropTransformation;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +32,17 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        ImageView ava = view.findViewById(R.id.ava);
+
+
+        Picasso.get()
+                .load(R.drawable.logo)
+                .transform(new CropCircleTransformation())
+                .into(ava);
+
+        return view;
     }
 
 }
