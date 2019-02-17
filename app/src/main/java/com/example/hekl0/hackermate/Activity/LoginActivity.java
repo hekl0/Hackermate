2360fd1a19afa4e7c69c7e8b4912fffc56fd1d3e
CallmeB <btran8@u.rochester.edu>
2019-02-17 05:47:02 -0500

    sign-in

--- app/src/main/java/com/example/hekl0/hackermate/Activity/LoginActivity.java
+++ app/src/main/java/com/example/hekl0/hackermate/Activity/LoginActivity.java
@@ -7,12 +7,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.example.hekl0.hackermate.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -21,7 +25,7 @@ import info.hoang8f.widget.FButton;
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "LoginActivity";
     FirebaseAuth mAuth;
-
+    EditText Email,Password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,17 +45,47 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                     public void onComplete(@NonNull Task<AuthResult> task) {
                     }
                 });
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-        Log.d(TAG, "onComplete: ok " + myRef);
+        Email = findViewById(R.id.username);
+        Password = findViewById(R.id.password);
 
-        myRef.setValue("Hello, World!");
+
+    }
+
+    public boolean checkValid(String email, String password) {
+        if(email.length() > 0 && password.length() > 0) {
+            return true;
+        }
+        return false;
     }
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.login_button) {
-            Log.d(TAG, "onClick: login button");
+            if(checkValid(Email.getText().toString(), Password.getText().toString())==true)
+            mAuth.signInWithEmailAndPassword(Email.getText().toString(), Password.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "signInWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                Toast.makeText(LoginActivity.this, "Wrong Username or Password!",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+
+                            // ...
+                        }
+                    });
+            else {
+                Toast.makeText(LoginActivity.this, "Your Username or Password is Invalid!",
+                        Toast.LENGTH_SHORT).show();
+            }
         }
         if (v.getId() == R.id.signup_button) {
             Intent intent = new Intent(this, SignUpActivity.class);
